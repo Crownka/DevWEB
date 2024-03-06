da16ec7a1f1855c7c8df801089b02b9bd119fbb7
Patrick <crownka0@gmail.com>
2024-03-06 08:51:15 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import ImprimeDados from "./componentes/00Atividades/02MeusDados/00MeusDados";
+import Temperatura from "./componentes/00Atividades/03Temperatura/00Temperatura";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <ImprimeDados
+                nome="Patrick de Farias Ramos"
+                mat="556711"
+                curso="Ciência da Computação"
+                univ="UFC - Campus Quixadá"
+            />
+            <hr />
+            <Temperatura celsius={100} fahrenheint={86} kelvinn={400} />
+        </div>
+    );
 }
 
 export default App;
