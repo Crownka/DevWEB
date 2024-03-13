74d49a315c9db889b2a1bc63069ebcf74702d83d
Patrick <crownka0@gmail.com>
2024-03-13 08:42:47 -0300

    PropDrilling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 //import ComPai from "./componentes/04ComPaiFilho/ComPai";
-import Pokemon from "./componentes/05Estados/Pokemon";
-
+//import Pokemon from "./componentes/05Estados/Pokemon";
+import {CompA} from "./componentes/06PropDrilling/PropDrilling";
 
 function App() {
     return (
         <div>
             <h1>Desenvolvimento de Software para Web!</h1>
 
-            <Pokemon />
+            <CompA />
         </div>
     );
 }
