255fa412f4ba2b46b92c95c92359ffc2dfbc1253
Patrick <crownka0@gmail.com>
2024-07-15 10:37:34 -0300

    SaveData DEV WEB

---
+++ src/componentes/14LocalSessionStorage/SaveLoadData.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+
+const SaveLoadData = () => {
+
+    const aluno = {
+        nome:"Fulano",
+        curso:"CC",
+        IRA : 7.5
+    }
+
+    // Variável de estado para armazenar os dados do aluno
+    const [alunoStorage, setAlunoStorage] = useState({dado:"vazio"})
+
+    /**
+     * localStorage - armazena dados no navegador do usuário
+     * localStorage.setItem("nome", "Fulano") - armazena o valor "Fulano" na chave "nome"
+     * localStorage.getItem("nome") - retorna o valor armazenado na chave "nome"
+     */
+    function saveData(){
+        localStorage.setItem("aluno", JSON.stringify(aluno))
+        alert("Dado salvo com sucesso")
+    }
+
+    function loadData(){
+        
+    }
+
+    return (
+        <div>
+            <h1>Save Load Data</h1>
+            <h3>
+                {
+                    JSON.stringify(alunoStorage)
+                }
+            </h3>
+            <button>
+                SAVE DATA
+            </button>
+            <button>
+                LOAD DATA
+            </button>
+        </div>
+    )
+}
+
+export default SaveLoadData
