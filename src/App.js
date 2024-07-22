0676ea6791bc42227da578dc00dc15eb7f5742fd
Patrick <crownka0@gmail.com>
2024-07-22 11:17:16 -0300

    Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import SaveLoadData from './componentes/14LocalSessionStorage/SaveLoadData';
+//import SaveLoadData from './componentes/14LocalSessionStorage/SaveLoadData';
+import MyNavbar from './componentes/16Bootstrap/MyNavbar';
 
 function App() {
   return (
     <div>
-      <SaveLoadData />
+      <MyNavbar />
     </div>
   );
 }
-
 export default App;
