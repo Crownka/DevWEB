ddeb943fd04884d7b75dfddcf1cdbac19e65e7fc
Patrick <crownka0@gmail.com>
2024-08-26 10:48:35 -0300

    criação do crud express (pasta nova em WEB)

---
+++ crud-express/models/ProfessorModel.js
@@ -0,0 +1,12 @@
+class ProfessorModel {
+    constructor() {
+        this.id = id
+        this.nome = nome
+        this.curso = curso
+        this.titulacao = titulacao
+        this.universidade = universidade
+        this.
+    }
+}
+
+module.exports = ProfessorModel
